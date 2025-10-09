a19e684185857af3e72c8eae53b7cef7d0aa593d
Andrew Souza <andrew.souza102024@gmail.com>
2025-10-09 13:11:18 -0300

    Criando o server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,17 @@
-import { Express } from "express";
+import http from "http";
+import { Server } from "socket.io";
+import app from "./app";
+import initSocket from "./config/socket";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+initSocket(io);
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => console.log(`🚀 Servidor rodando na porta ${PORT}`));
